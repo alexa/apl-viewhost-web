7cb794e2727868916e0dd067d716809cd2f11467
Luis Valencia <luvle@amazon.com>
2021-05-12 14:41:26 -0700

    May 2021 Patch Release: APL 1.6.3 APL Viewhost Web

---
+++ scripts/fetch.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const https = require('https');
+const fs = require('fs');
+
+const artifactUrl = 'https://d1gkjrhppbyzyh.cloudfront.net/apl-viewhost-web/3173e22c-306d-4703-9b18-8cbb64f7871c/index.js';
+
+const outputFilePath = 'index.js';
+const outputFile = fs.createWriteStream(outputFilePath);
+
+https.get(artifactUrl, (response) => {
+    response.pipe(outputFile);
+}).on('error', (e) => {
+    throw Error(`Failed to fetch apl-viewhostweb-artifact: ${e}`);
+});
